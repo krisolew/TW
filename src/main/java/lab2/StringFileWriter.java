50efd5349141c667ab88ca167902be406288f9ca
anqe <banaszak.ab@gmail.com>
2019-10-23 15:22:41 +0100

    add FileWriter

---
+++ src/main/java/lab2/StringFileWriter.java
@@ -0,0 +1,17 @@
+package main.java.lab2;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class StringFileWriter {
+
+    private FileWriter fileWriter;
+
+    public StringFileWriter(String fileName) throws IOException {
+        this.fileWriter = new FileWriter(fileName);
+    }
+
+    public void write(String content) throws IOException {
+        fileWriter.write(content);
+    }
+}
