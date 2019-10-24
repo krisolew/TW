c3a310db5bb542f07108afeb1d4721201e55d4b4
Krzysztof Olewiński <krisolew@wp.pl>
2019-10-24 02:21:21 +0200

    Added multiple configurations

--- src/main/java/lab2/Main.java
+++ src/main/java/lab2/Main.java
@@ -1,7 +1,5 @@
 package main.java.lab2;
 
-import main.java.lab2.bufor.BuforType;
-import main.java.lab2.configuration.LikelihoodType;
 import main.java.lab2.configuration.RunConfiguration;
 import main.java.lab2.threads.MyThread;
 import main.java.lab2.threads.ThreadType;
@@ -12,32 +10,36 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        RunConfiguration configuration = new RunConfiguration("src/main/resources/lab2/times.txt", 1000, 1000, LikelihoodType.CONSTANT, BuforType.DUMMY);
-
+        List<RunConfiguration> configurations = RunConfiguration.getRunConfigurations();
         List<MyThread> producer = new LinkedList<>();
         List<MyThread> consumer = new LinkedList<>();
 
-        for (int i = 0; i < configuration.numOfThreads; i++) {
-            producer.add(new MyThread(configuration, ThreadType.PRODUCER));
-            consumer.add(new MyThread(configuration, ThreadType.CONSUMER));
-        }
+        for (RunConfiguration configuration : configurations) {
+            for (int i = 0; i < configuration.numOfThreads; i++) {
+                producer.add(new MyThread(configuration, ThreadType.PRODUCER));
+                consumer.add(new MyThread(configuration, ThreadType.CONSUMER));
+            }
 
-        consumer.forEach(Thread::start);
-        producer.forEach(Thread::start);
+            producer.forEach(Thread::start);
+            consumer.forEach(Thread::start);
 
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-        consumer.forEach(Thread::stop);
-        producer.forEach(Thread::stop);
+            producer.forEach(Thread::stop);
+            consumer.forEach(Thread::stop);
 
-        try {
-            configuration.writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            try {
+                configuration.writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            producer.clear();
+            consumer.clear();
         }
     }
 }
--- src/main/java/lab2/configuration/RunConfiguration.java
+++ src/main/java/lab2/configuration/RunConfiguration.java
@@ -7,10 +7,16 @@ import main.java.lab2.bufor.DummyBufor;
 import main.java.lab2.threads.ThreadType;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 public class RunConfiguration {
-    private final static Random random = new Random();
+    private final static Random RANDOM = new Random();
+    private final static String PACKAGE_PATH = "src/main/resources/lab2/";
+    private final static String FILE_NAME_PATTERN = "times_.txt";
+    private static Integer FILE_NUMBER = 0;
+
     public AbstractBufor bufor;
     public StringFileWriter writer;
     public int capacity;
@@ -22,28 +28,74 @@ public class RunConfiguration {
         this.bufor = buforType == BuforType.DUMMY ? new DummyBufor(capacity) : new AlgoBufor(capacity);
         this.capacity = capacity;
         this.numOfThreads = numOfThreads;
-        this.writer = new StringFileWriter(file);
+        this.writer = new StringFileWriter(PACKAGE_PATH + file);
         this.range = capacity/2;
         this.likelihoodType = likelihoodType;
     }
 
     public static int getRandomPortion(ThreadType type, int range) {
-        return type.getValue() * random.nextInt(range);
+        return type.getValue() * RANDOM.nextInt(range);
     }
 
     public static int getRandomPortionWithVariableLikelihood(ThreadType type, int range) {
-        int param = random.nextInt(range);
+        int param = RANDOM.nextInt(range);
         if (param < (range/2)) {
-            return type.getValue() * random.nextInt(range / 5);
+            return type.getValue() * RANDOM.nextInt(range / 5);
         }
         else if (param < (3 * range / 4)) {
-            return type.getValue() * random.nextInt(range / 4);
+            return type.getValue() * RANDOM.nextInt(range / 4);
         }
         else if (param < (19 * range / 20)) {
-            return type.getValue() * random.nextInt(range / 3);
+            return type.getValue() * RANDOM.nextInt(range / 3);
         }
         else {
-            return type.getValue() * random.nextInt(range);
+            return type.getValue() * RANDOM.nextInt(range);
         }
+    }
+
+    public static List<RunConfiguration> getRunConfigurations() throws IOException {
+        List<RunConfiguration> configurations = new LinkedList<>();
+
+        configurations.add(new RunConfiguration(getFileName(), 10000, 100,
+                LikelihoodType.CONSTANT, BuforType.DUMMY));
+        configurations.add(new RunConfiguration(getFileName(), 10000, 100,
+                LikelihoodType.CONSTANT, BuforType.ALGO));
+        configurations.add(new RunConfiguration(getFileName(), 10000, 100,
+                LikelihoodType.VARIABLE, BuforType.DUMMY));
+        configurations.add(new RunConfiguration(getFileName(), 10000, 100,
+                LikelihoodType.VARIABLE, BuforType.ALGO));
+
+        configurations.add(new RunConfiguration(getFileName(), 10000, 1000,
+                LikelihoodType.CONSTANT, BuforType.DUMMY));
+        configurations.add(new RunConfiguration(getFileName(), 10000, 1000,
+                LikelihoodType.CONSTANT, BuforType.ALGO));
+        configurations.add(new RunConfiguration(getFileName(), 10000, 1000,
+                LikelihoodType.VARIABLE, BuforType.DUMMY));
+        configurations.add(new RunConfiguration(getFileName(), 10000, 1000,
+                LikelihoodType.VARIABLE, BuforType.ALGO));
+
+        configurations.add(new RunConfiguration(getFileName(), 100000, 100,
+                LikelihoodType.CONSTANT, BuforType.DUMMY));
+        configurations.add(new RunConfiguration(getFileName(), 100000, 100,
+                LikelihoodType.CONSTANT, BuforType.ALGO));
+        configurations.add(new RunConfiguration(getFileName(), 100000, 100,
+                LikelihoodType.VARIABLE, BuforType.DUMMY));
+        configurations.add(new RunConfiguration(getFileName(), 100000, 100,
+                LikelihoodType.VARIABLE, BuforType.ALGO));
+
+        configurations.add(new RunConfiguration(getFileName(), 100000, 1000,
+                LikelihoodType.CONSTANT, BuforType.DUMMY));
+        configurations.add(new RunConfiguration(getFileName(), 100000, 1000,
+                LikelihoodType.CONSTANT, BuforType.ALGO));
+        configurations.add(new RunConfiguration(getFileName(), 100000, 1000,
+                LikelihoodType.VARIABLE, BuforType.DUMMY));
+        configurations.add(new RunConfiguration(getFileName(), 100000, 1000,
+                LikelihoodType.VARIABLE, BuforType.ALGO));
+        return configurations;
+    }
+
+    private static String getFileName(){
+        FILE_NUMBER++;
+        return FILE_NAME_PATTERN.replace("_", FILE_NUMBER.toString());
     }
 }
